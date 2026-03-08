3847edf152fb7ddee939a9a77b7b50be8c14c097
bey <167071840+beybasa@users.noreply.github.com>
2026-03-08 13:18:36 +0100

    Initial Commit

---
+++ DeepScan/DeepScanApp.swift
@@ -0,0 +1,32 @@
+//
+//  DeepScanApp.swift
+//  DeepScan
+//
+//  Created by Jason Nguyen on 08.03.2026.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct DeepScanApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ DeepScanTests/DeepScanTests.swift
@@ -0,0 +1,17 @@
+//
+//  DeepScanTests.swift
+//  DeepScanTests
+//
+//  Created by Jason Nguyen on 08.03.2026.
+//
+
+import Testing
+@testable import DeepScan
+
+struct DeepScanTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
